4569a38d2c31bdbbceb369e4012a6f8cd9a547a0
derekzyl <derekzyl@gmail.com>
2024-11-14 21:40:28 +0100

    Refactor project structure and remove unused code

--- modules/general_crud/crud_utils.py
+++
@@ -1,7 +0,0 @@
-def getList(select: str) -> tuple[list | None, list | None]:
-    if select is None:
-        return [], []
-    get_list = [field.strip() for field in select.split(",")]
-    add_field = [field[1:] for field in get_list if not field.startswith("-")]
-    remove_field = [field[1:] for field in get_list if field.startswith("-")]
-    return add_field, remove_field

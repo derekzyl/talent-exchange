16a2a2a343c15c16c8707838f07edd3ae7f19255
derekzyl <derekzyl@gmail.com>
2025-01-13 17:18:40 +0100

    Add initial implementation for skills, reviews, and skill share modules

---
+++ app/core/skills/types/types_skills.py
@@ -0,0 +1,86 @@
+from tkinter import E
+from typing import TypedDict
+from enum import Enum
+
+
+class enum_skill_level(Enum, str):
+    beginner = "beginner"
+    intermediate = "intermediate"
+    advanced = "advanced"
+    expert = "expert"
+
+
+class enum_available_day(Enum, str):
+    monday = "monday"
+    tuesday = "tuesday"
+    wednesday = "wednesday"
+    thursday = "thursday"
+    friday = "friday"
+    saturday = "saturday"
+    sunday = "sunday"
+class SkillT(TypedDict):
+    id:str
+    skill_category: str
+    skill_name: str
+    skill_sub_category: str
+    skill_description: str
+    skill_level: enum_skill_level
+    skill_experience: str
+    
+    user_id:str
+    created_at:str
+    updated_at:str
+    deleted_at:str
+
+class CreateSkillT(TypedDict):
+    skill_category: str
+    skill_name: str
+    skill_sub_category: str
+    skill_description: str
+    skill_level: enum_skill_level
+    skill_experience: str
+    user_id:str
+
+
+class UpdateSkillT(TypedDict):
+    skill_category: str
+    skill_name: str
+    skill_sub_category: str
+    skill_description: str
+    skill_level: enum_skill_level
+    skill_experience: str
+    user_id:str
+    id:str
+
+class DeleteSkillT(TypedDict):
+    id:str
+    user_id:str            
+
+
+class AvailableTimeT(TypedDict):
+    id:str
+    skill_id:str
+    available_day:str
+    start_time:str
+    end_time:str
+    created_at:str
+    updated_at:str
+    deleted_at:str
+
+class CreateAvailableTimeT(TypedDict):
+    skill_id:str
+    available_day:str
+    start_time:str
+    end_time:str
+
+class UpdateAvailableTimeT(TypedDict):
+    skill_id:str
+    available_day:enum_available_day    
+    start_time:str
+    end_time:str
+    id:str
+
+class DeleteAvailableTimeT(TypedDict):
+    id:str
+    skill_id:str
+    
